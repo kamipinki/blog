278586eebffe8d07fef972782f8a90261314ad0d
kamipinki <kamipink@gmail.com>
2021-08-27 11:28:27 -0400

    Validacion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,13 +13,17 @@ class ArticlesController < ApplicationController
     end 
 
     def new
+      @article = Article.new
     end
     def create
         @article = Article.new(article_params)
-       
-        @article.save
-        
-        redirect_to @article
+
+        if @article.save
+       redirect_to articles_path(@article)
+         
+        else 
+          render :new
+        end
       end
        
       private
