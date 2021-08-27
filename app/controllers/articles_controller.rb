18e498a4eb5f8248053f09bfa598ca8cdbc82b8f
kamipinki <kamipink@gmail.com>
2021-08-27 10:02:36 -0400

    Agregando edit de articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,15 @@ class ArticlesController < ApplicationController
     def index
          @articles = Article.all
     end
+    
     def show
         @article = Article.find(params[:id])
     end
+
+    def edit 
+      @article = Article.find(params[:id])
+    end 
+
     def new
     end
     def create
