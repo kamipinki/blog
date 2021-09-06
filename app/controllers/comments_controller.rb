f058ffc74ce7c1ef62897bf1f4035d77d8c797a0
kamipinki <kamipink@gmail.com>
2021-09-06 20:45:21 -0300

    simplificando controlador comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,19 @@
 class CommentsController < ApplicationController
+  before_action :find_article, only: [:create, :destroy]
     def create
-        @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
       end
       def destroy
-        @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
         redirect_to article_path(@article)
       end
       private
+      def find_article 
+        @article = Article.find(params[:article_id])
         def comment_params
           params.require(:comment).permit(:commenter, :body)
         end
+      end
     end
