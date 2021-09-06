1bbbf3943c7b05306b22c3269bbcad358d114777
kamipinki <kamipink@gmail.com>
2021-09-06 20:54:07 -0300

    color

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles do
-  resources :comments
+  resources :comments, only: [:create, :destroy]
 end
 end
