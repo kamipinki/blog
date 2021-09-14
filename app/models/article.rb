0b38606967655b655e84b36d8bad55a5edee4a14
kamipinki <kamipink@gmail.com>
2021-09-14 20:18:46 -0300

    cambios

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
     validates :title, presence: true,
                       length: { minimum: 5 }
+before_save :mayuscula
+after_save :enviar_notificacion
+def mayuscula
+    self.title = self.title.upcase
+    end
   end
