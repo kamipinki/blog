207b59635155ffe8533cac83960724ed08f70b07
kamipinki <kamipink@gmail.com>
2021-09-15 18:48:32 -0300

    arreglandovistas3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,50 +1,51 @@
 class ArticlesController < ApplicationController
-before_action :find_article, only:[:show,:edit, :update, :destroy]
-    def index
-         @articles = Article.all
+  before_action :find_article, only: [:show,:edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+     redirect_to article_path(@article)
+    else
+     render :new
     end
-    
-    def show
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
+  end
 
-    def edit 
-    end 
+  def destroy
+    @article.destroy
 
-    def new
-      @article = Article.new
-    end
-    def create
-        @article = Article.new(article_params)
+    redirect_to articles_path
+  end
 
-        if @article.save
-       redirect_to articles_path(@article)
-         
-        else 
-          render :new
-        end
-      end
-      def update
-        
-      if @article.update(article_params)
-        redirect_to @article
-      else
-        render :edit
-      end
+private
 
-      def destroy
-      
-        @article.destroy
-     
-        redirect_to articles_path
-      end
-     
-
-    end
-      private
-      def find_article
-        @article = Article.find(params[:id])
-      end
-        def article_params
-          params.require(:article).permit(:title, :text, :photo)
-        end
+  def find_article
+    @article = Article.find(params[:id])
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :text, :photo)
+  end
 end
